a38e9ed0f2c9fb81d7f802d2cee6914f87c8b32a
Deqing Huang <deqing.huang@anz.com>
2024-04-14 02:08:36 +1000

    ok

---
+++ batchprocessor.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"fmt"
+	. "github.com/deqinganz/batching-api/api"
+	"log"
+)
+
+type BatchProcessor struct{}
+
+// Process is one method of BatchProcessor which should be implemented externally
+func (b *BatchProcessor) Process(jobs []Job) {
+	for i := range jobs {
+		jobs[i].Status = SUBMITTED
+	}
+
+	var output string
+	for _, job := range jobs {
+		output = output + fmt.Sprintf("[%s %s \"%s\"] ", job.Id.String()[:8], job.Type, job.Name)
+	}
+	log.Printf("Processed jobs: %s", output)
+}
---
+++ batchprocessor_test.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	. "github.com/deqinganz/batching-api/api"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestProcess(t *testing.T) {
+	jobs := []Job{{Status: QUEUED}, {Status: QUEUED}}
+	bp := BatchProcessor{}
+	bp.Process(jobs)
+
+	assert.Equal(t, []Job{{Status: SUBMITTED}, {Status: SUBMITTED}}, jobs)
+}
